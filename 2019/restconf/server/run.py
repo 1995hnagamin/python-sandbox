db8f4bbb6bd72497de5a376b4f23f13b686fcf4d
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2019-11-29 19:31:06 +0900

    Add 2019/restconf/server/run.py

---
+++ 2019/restconf/server/run.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import textwrap
+
+app = Flask(__name__)
+
+@app.route('/.well-known/host-meta', methods=['GET'])
+def func():
+    msg = """\
+            <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
+                <Link rel='restconf' href='/restconf'/>
+            </XRD>
+            """
+    return textwrap.dedent(msg)
+
+if __name__ == "__main__":
+    app.debug = True
+    portnum = 8080
+    app.run(host='0.0.0.0', port=portnum)
