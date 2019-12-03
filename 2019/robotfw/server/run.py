c92fdd875698fd456d3c0457875100dd87d30a84
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2019-12-03 21:14:07 +0900

    Implement `/upload`

--- 2019/robotfw/server/run.py
+++ 2019/robotfw/server/run.py
@@ -2,6 +2,7 @@ import flask
 import json
 import os
 import textwrap
+import uuid
 
 app = flask.Flask(__name__)
 
@@ -23,6 +24,16 @@ def show_image_list():
     return json.dumps(
         [image_info(os.path.splitext(p)[0]) for p in os.listdir("data/img")]
     )
+
+
+@app.route("/upload", methods=["POST"])
+def receive_image():
+    if "image" not in flask.request.files:
+        return json.dumps({"status": "error", "message": "image missing."})
+    data = flask.request.files["image"]
+    img_id = str(uuid.uuid1())
+    data.save(os.path.join("data/img", img_id))
+    return json.dumps({"status": "ok", "image_id": img_id})
 
 
 if __name__ == "__main__":
